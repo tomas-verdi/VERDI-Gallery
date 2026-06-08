2c5b6e556e10b88c43cec40baa7fe6d8f698b4db
Tomas Vera <tomasvera@Tomass-Mac-mini.local>
2026-06-08 13:01:32 -0500

    restore service worker v8

--- sw.js
+++ sw.js
@@ -1,2 +1,25 @@
-// Service worker disabled
-self.addEventListener('fetch', e => {});
+const CACHE = 'verdi-v8';
+const ASSETS = ['/'];
+
+self.addEventListener('install', e => {
+  self.skipWaiting();
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    ).then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    fetch(e.request).then(res => {
+      const clone = res.clone();
+      caches.open(CACHE).then(c => c.put(e.request, clone));
+      return res;
+    }).catch(() => caches.match(e.request))
+  );
+});
